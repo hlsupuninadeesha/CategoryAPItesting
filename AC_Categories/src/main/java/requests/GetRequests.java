767f4a40a50fc1346b9974a6e7075844f6e32f46
SupuniN <SupuniN@SupuniN.seranet.local>
2022-06-13 21:59:35 +0530

    My first commit

--- AC_Categories/src/main/java/requests/GetRequests.java
+++ AC_Categories/src/main/java/requests/GetRequests.java
@@ -5,6 +5,13 @@ import io.restassured.response.Response;
 
 public class GetRequests {
 
+	/* * These methods are used to pass the URL parameters and return json response in order to assert at run time
+	   * This pattern can be used with other CURD operations to increase the usability and mainatainability of the code
+	   * Requirement was only to seert get response for now but can further develop this for other requests as well.
+	  
+	  
+	*/
+	
 	public Response getName(String baseURI,String basePath)
 	{
 
@@ -27,7 +34,6 @@ public class GetRequests {
 		System.out.println("Get promotions running");
 		return response;
 	}
-
 
 
 	public Response setURI(String baseURI,String basePath)
--- AC_Categories/src/main/java/tests/TestsGetRequests.java
+++ AC_Categories/src/main/java/tests/TestsGetRequests.java
@@ -11,29 +11,44 @@ import utility.JSONReader;
 
 public class TestsGetRequests {
 
+	/*
+	 * This is to run the project with testNG annotations
+	 *  
+	 *   
+	 *   
+	 *   
+	 *   
+	 *   */
 	@Test
 	public void runTests() throws FileNotFoundException, IOException, ParseException
 	{
-
+        //Create an object from json reader file
 		JSONReader jsonreader = new JSONReader();
+		//Read the json file from the given path and access node value and return as a string
 		String baseURI = jsonreader.readJSONFile(".\\data\\wsData.json","BaseURI");
 		String basePath = jsonreader.readJSONFile(".\\data\\wsData.json","BasePath");
 
-
+        //Create object from the class where all get requests implemented
 		GetRequests getrequests = new GetRequests();
-		//Response response = getrequests.setURI(baseURI,basePath);
-		Response responsename = getrequests.getName(baseURI,basePath);
-		responsename.then().assertThat().body("Name",equalTo("Carbon credits"));
+		
+		/* Either can create a single object or create object at each time 
+		 * 
+		 */
+		
+		
+		//Asserting the body parameters in JSON get response
+		Response response = getrequests.getName(baseURI,basePath);
+		response.then().assertThat().body("Name",equalTo("Carbon credits"));
 		System.out.println("Name Acceptance criteria is a sucess");
 
-
-		Response responsecanlist = getrequests.getCanrelist(baseURI,basePath);
-		responsecanlist.then().assertThat().body("CanRelist",equalTo(true));
+		//Asserting the body parameters in JSON get response
+		//Response responsecanlist = getrequests.getCanrelist(baseURI,basePath);
+		response.then().assertThat().body("CanRelist",equalTo(true));
 		System.out.println("CanRelist Acceptance criteria is a sucess");
 
-
-		Response  responsepromotion = getrequests.getPromotions(baseURI,basePath);
-		responsepromotion.then().assertThat().body("Promotions[1].Name",equalTo("Gallery"),"Promotions[1].Description",equalTo("Good position in category"));		
+		//Asserting the body parameters in JSON get response
+		//Response  responsepromotion = getrequests.getPromotions(baseURI,basePath);
+		response.then().assertThat().body("Promotions[1].Name",equalTo("Gallery"),"Promotions[1].Description",equalTo("Good position in category"));		
 		System.out.println("Promotions Acceptance criteria is a sucess");
 
 	}
